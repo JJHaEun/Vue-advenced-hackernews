0bb3579fa2ebee03036f6a53fe13fbb3ebce84c0
JJHaEun <yj05037@gmail.com>
2023-07-07 15:34:08 +0900

    헤더 컴포넌트(tool-bar)만들고 router-link사용하기

--- vue-news/src/main.js
+++ vue-news/src/main.js
@@ -6,5 +6,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
+  // router라는 인스턴스 옵션에 라우터 연결 => router:router => 축약하면 router이됨.
   router,
 }).$mount("#app");
--- vue-news/src/router/index.js
+++ vue-news/src/router/index.js
@@ -7,7 +7,14 @@ import AskView from "../views/AskView.vue";
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
+  mode: "history", // nomal한 url생성하기위해 추가해줌.
   routes: [
+    {
+      path: "/",
+
+      // 첫 접속하자마자 보여줄 페이지 주소
+      redirect: "/news",
+    },
     {
       // path 에는 url 정보가(url주소),
       // component에는 해당 컴포넌트가 들어감.(url주소로 이동했을때 표시할 컴포넌트(페이지))
