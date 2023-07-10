89eec72f65d5dc3007297f62696b4aa192f7348c
JJHaEun <yj05037@gmail.com>
2023-07-10 16:42:48 +0900

    axios로 api호출하기

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>news</div>
+  <div>
+    <div v-for="(news, i) in news" :key="i">{{ news.title }}</div>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      news: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://api.hnpwa.com/v0/news/1.json")
+      .then(res => {
+        console.log(res.data);
+        this.news = res.data;
+      })
+      .catch(error => console.log(error));
+  },
+};
 </script>
 
 <style></style>
